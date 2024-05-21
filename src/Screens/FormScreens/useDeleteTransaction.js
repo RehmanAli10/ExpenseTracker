fd64aaca556b5f8f6000709d5d11b317723a8e66
RehmanAli10 <vrehmanali@gmail.com>
2024-05-21 21:31:40 +0500

    added delete transaction

---
+++ src/Screens/FormScreens/useDeleteTransaction.js
@@ -0,0 +1,34 @@
+import {useQueryClient, useMutation} from '@tanstack/react-query';
+import {useToast} from 'react-native-toast-notifications';
+
+import {deleteTransaction as deleteTransactionApi} from '../../Services/apiTransactions';
+
+export function useDeleteTransaction() {
+  const queryClient = useQueryClient();
+
+  const toast = useToast();
+
+  const {isLoading: isDeleting, mutate: deleteTransaction} = useMutation({
+    mutationFn: deleteTransactionApi,
+
+    onSuccess: () => {
+      toast.show('Transaction deleted successfully ', {
+        type: 'success',
+        placement: 'top',
+        duration: 2000,
+      });
+
+      queryClient.invalidateQueries({
+        queryKey: ['transactions'],
+      });
+    },
+    onError: err =>
+      toast.show(err.message, {
+        type: 'danger',
+        placement: 'top',
+        duration: 2000,
+      }),
+  });
+
+  return {isDeleting, deleteTransaction};
+}
