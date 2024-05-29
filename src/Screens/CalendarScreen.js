b84c555105b375fa7c70f2da7b3c9fa8db4e17e5
RehmanAli10 <vrehmanali@gmail.com>
2024-05-29 23:46:45 +0500

    displayed transactions on selected date

--- src/Screens/CalendarScreen.js
+++ src/Screens/CalendarScreen.js
@@ -1,50 +1,117 @@
-import React, {useState} from 'react';
-import {StyleSheet, Text, View, FlatList} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {StyleSheet, View, FlatList, Text} from 'react-native';
 import {Calendar} from 'react-native-calendars';
 import {useTransactions} from './TransactionScreen/useTransactions';
+import {useDeleteTransaction} from './FormScreens/useDeleteTransaction';
+import Transaction from '../Components/Transaction';
+import {useNavigation} from '@react-navigation/native';
 
 function CalendarScreen() {
-  const [selected, setSelected] = useState('');
-  const [selectedTransactions, setSelectedTransactions] = useState([]);
-  const {transactions} = useTransactions();
+  const navigation = useNavigation();
 
-  const transactionDates = [];
-  for (const month in transactions) {
-    transactions[month].forEach(transaction => {
-      const date = transaction.time.split('T')[0];
-      transactionDates.push(date);
-    });
+  const [selected, setSelected] = useState(new Date());
+  const [modalVisible, setModalVisible] = useState(false);
+  const [detailDescription, setDetailDescription] = useState('');
+  const [deleteModalVisible, setDeleteModalVisible] = useState(false);
+  const [selectedTransaction, setSelectedTransaction] = useState(null);
+  const [selectedTransactions, setSelectedTransactions] = useState([]);
+
+  const {transactions, refetch} = useTransactions();
+  const {deleteTransaction} = useDeleteTransaction();
+
+  let updatedTransactions = Object.values(transactions).flat();
+
+  useEffect(
+    function () {
+      const currentDate = new Date().toISOString().split('T')[0];
+
+      const currentTransaction = updatedTransactions.filter(currTrans => {
+        const date = currTrans.time.split('T')[0];
+        return date === currentDate;
+      });
+
+      setSelectedTransactions(currentTransaction);
+    },
+    [selected],
+  );
+
+  function handleEdit(id) {
+    let newData = Object.values(transactions)
+      .flat(1)
+      .find(currEle => currEle.id === id);
+
+    let editedData = {
+      id: newData.id,
+      amount: newData.amount,
+      description: newData.description,
+      time: newData.time,
+    };
+
+    if (newData.type === 'income') {
+      navigation.navigate('IncomeFormScreen', {
+        index: editedData,
+      });
+    } else {
+      navigation.navigate('ExpenseFormScreen', {
+        index: editedData,
+      });
+    }
   }
 
-  const markedDates = transactionDates.reduce((acc, date) => {
-    acc[date] = {
-      marked: true,
-      dotColor: 'orange',
-    };
-    return acc;
-  }, {});
+  function handleDelete(id) {
+    deleteTransaction(id);
+    const deleteTransactions = selectedTransactions.filter(
+      currTra => currTra.id !== id,
+    );
+    setSelectedTransactions(deleteTransactions);
+    refetch();
+  }
 
-  if (selected) {
-    markedDates[selected] = {
-      selected: true,
-      selectedColor: 'blue',
-      disableTouchEvent: true,
-    };
+  function handleModal(desc, id) {
+    setSelectedTransaction(id);
+    setDetailDescription(desc);
+    setModalVisible(true);
   }
 
   const handleDayPress = day => {
     setSelected(day.dateString);
+  };
 
-    const transactionsForSelectedDate = [];
-    for (const month in transactions) {
-      transactions[month].forEach(transaction => {
-        const date = transaction.time.split('T')[0];
-        if (date === day.dateString) {
-          transactionsForSelectedDate.push(transaction);
-        }
-      });
-    }
-    setSelectedTransactions(transactionsForSelectedDate);
+  const markedDates = {};
+
+  updatedTransactions.forEach(transaction => {
+    const date = transaction.time.split('T')[0];
+    markedDates[date] = {
+      selected: true,
+      marked: true,
+      selectedColor: 'blue',
+    };
+  });
+
+  const filteredTransactions = updatedTransactions.filter(transaction => {
+    const date = transaction.time.split('T')[0];
+    return date === selected;
+  });
+
+  const renderItem = ({item}) => {
+    return (
+      <Transaction
+        id={item.id}
+        amount={item.amount}
+        type={item.type}
+        description={item.description}
+        time={item.time}
+        handleEdit={handleEdit}
+        handleDelete={handleDelete}
+        handleModal={handleModal}
+        modalVisible={modalVisible}
+        setModalVisible={setModalVisible}
+        detailDescription={detailDescription}
+        deleteModalVisible={deleteModalVisible}
+        setDeleteModalVisible={setDeleteModalVisible}
+        selectedTransaction={selectedTransaction}
+      />
+    );
   };
 
   return (
@@ -57,20 +124,15 @@ function CalendarScreen() {
         }}
       />
       <View style={styles.transactionList}>
-        <Text style={styles.title}>Transactions for {selected}:</Text>
-        <FlatList
-          data={selectedTransactions}
-          keyExtractor={item => item.id.toString()}
-          renderItem={({item}) => (
-            <View style={styles.transactionItem}>
-              <Text>
-                {item.description}: {item.amount}
-              </Text>
-              <Text>Type: {item.type}</Text>
-              <Text>Time: {new Date(item.time).toLocaleString()}</Text>
-            </View>
-          )}
-        />
+        {filteredTransactions.length > 0 ? (
+          <FlatList
+            data={filteredTransactions}
+            keyExtractor={item => item.id.toString()}
+            renderItem={renderItem}
+          />
+        ) : (
+          <Text></Text>
+        )}
       </View>
     </View>
   );
@@ -82,9 +144,9 @@ const styles = StyleSheet.create({
   transactionList: {
     marginTop: 20,
     padding: 10,
-    backgroundColor: 'black',
   },
   title: {
+    color: 'white',
     fontSize: 18,
     fontWeight: 'bold',
     marginBottom: 10,
@@ -93,5 +155,8 @@ const styles = StyleSheet.create({
     padding: 10,
     borderBottomWidth: 1,
     borderBottomColor: '#ccc',
+  },
+  text: {
+    color: 'white',
   },
 });
