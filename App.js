41db2ecf32486086e73636f6e358902822a3a4d2
RehmanAli10 <vrehmanali@gmail.com>
2024-06-04 11:47:45 +0500

    refactored

--- App.js
+++ App.js
@@ -1,27 +1,39 @@
 import React, {useEffect, useState} from 'react';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import {ToastProvider} from 'react-native-toast-notifications';
 
 import Navigation from './src/Navigation';
 import SplashScreen from './src/Components/SplashScreen';
 
-// const queryClient = new QueryClient({
-//   defaultOptions: {
-//     queries: {
-//       staleTime: 0,
-//     },
-//   },
-// });
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 0,
+    },
+  },
+});
 
 function App() {
   const [isSplashVisible, setIsSplashVisible] = useState(true);
 
-  useEffect(function () {
-    setTimeout(function () {
+  useEffect(() => {
+    const timer = setTimeout(() => {
       setIsSplashVisible(false);
     }, 3000);
+    return () => clearTimeout(timer);
   }, []);
 
-  return isSplashVisible ? <SplashScreen /> : <Navigation />;
+  return (
+    <ToastProvider
+      successColor="green"
+      duration={1000}
+      placement="top"
+      warningColor="darkred">
+      <QueryClientProvider client={queryClient}>
+        {isSplashVisible ? <SplashScreen /> : <Navigation />}
+      </QueryClientProvider>
+    </ToastProvider>
+  );
 }
 
 export default App;
